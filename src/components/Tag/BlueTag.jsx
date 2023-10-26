beab089f63cb6ac387f67ebec1497811b169acba
Animesh-Chakrabarty <animesh_chakrabarty@outlook.com>
2023-10-26 20:05:16 +0530

    commit

--- src/components/Tag/BlueTag.jsx
+++ src/components/Tag/BlueTag.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BlueTag = ({ tagTitle }) => {
   return (
-    <div className="bg-[#0071BD] w-fit px-4 py-1 rounded-[6px] font-robotoSlab text-white font-[500] ">
+    <div className="bg-[#0071BD] w-fit px-4 py-1 rounded-[6px] font-lato text-white font-[500] ">
       {tagTitle}
     </div>
   );
