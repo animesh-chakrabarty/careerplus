e65d2730dc459eb9e33354e430c0544fc8996ed1
Animesh-Chakrabarty <animesh_chakrabarty@outlook.com>
2023-10-30 22:01:14 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import SearchResult from "../components/SearchResult";
 const Home = () => {
   return (
     <div className=" py-2 px-4 max-md:px-0 h-[90%] overflow-auto no-scrollbar">
-      <p className="text-center text-5xl max-md:text-3xl leading-[60px] font-[700]">
+      <p className="text-center text-5xl max-md:text-3xl leading-[60px] font-[500] font-robotoSlab">
       Uniting Jobs from <br />Every Major Portal for You with <br />
         <span className="text-7xl max-md:text-5xl leading-[80px] blaugrana_gradient ">
           CareerPlus
