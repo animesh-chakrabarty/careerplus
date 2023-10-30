f5738089300d3e64b8c88d56a2bdb8d3aca2db5a
Animesh-Chakrabarty <animesh_chakrabarty@outlook.com>
2023-10-30 22:09:28 +0530

    commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,21 +3,34 @@ import { BsFillBookmarkFill } from "react-icons/bs";
 import { MdOutlineDownloadDone } from "react-icons/md";
 import { Link } from "react-router-dom";
 
+import { SiGithub } from "react-icons/si";
+
 const Header = () => {
   return (
-    <div className="h-[6%] flex items-center justify-between px-4 border-b-2 mt-1 ">
+    <div className="h-[6%] max-md:h-[4%] flex items-center justify-between px-4 border-b-2 mt-1 ">
       <Link to="/" className="h-full pb-1">
         <img src={logo} alt="" className="h-[100%]" />
       </Link>
-      <div className="flex items-center gap-10 mx-5">
+      {/* Saved & Applied section for md and above */}
+      <div className="flex items-center gap-10 mx-5 max-md:hidden">
         <Link to="/bookmark" className="flex flex-col items-center">
-          <BsFillBookmarkFill size={25} />
+          <BsFillBookmarkFill size={25} className="text-[25px]" />
           <p>Saved</p>
         </Link>
         <Link to="/applied" className="flex flex-col items-center">
           <MdOutlineDownloadDone size={30} />
           <p>Applied</p>
         </Link>
+      </div>
+      {/* Github Button for below md */}
+      <div>
+        <SiGithub
+          onClick={() => {
+            window.open(
+              "https://github.com/Animesh-Chakrabarty/Project-Job-Search-Portal"
+            );
+          }} size={25}
+        />
       </div>
     </div>
   );
