63f07de4d33b2636e03da75f66a8c85419233253
Animesh-Chakrabarty <animesh_chakrabarty@outlook.com>
2023-11-04 17:44:57 +0530

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,38 @@ import SearchResult from "../components/SearchResult";
 
 const Home = () => {
   return (
-    <div className=" py-2 px-4 max-md:px-0 h-[90%] max-md:h-[89%] overflow-hidden no-scrollbar max-md:mt-20">
-      <p className="text-center text-5xl max-md:text-3xl leading-[60px] font-[500] font-robotoSlab ">
-      Uniting Jobs from <br />Every Major Portal <br className="md:hidden"/>for You with <br />
+    <div className=" py-2 px-4 max-md:px-0 h-[90%] max-md:h-[89%] overflow-hidden no-scrollbar ">
+      <p className="text-center text-5xl max-md:text-3xl max-md:hidden leading-[60px] font-[500] font-robotoSlab ">
+        Uniting Jobs from <br />
+        Every Major Portal <br className="md:hidden" />
+        for You with <br />
         <span className="text-7xl max-md:text-5xl leading-[80px] blaugrana_gradient ">
           CareerPlus
         </span>
       </p>
       <SearchBox />
+      <div className="text-[20px] text-center font-[600] mt-5 font-lato">
+        <p className="text-gray-600 ">
+          Here at <span className="blaugrana_gradient">CareerPlus</span> ,{" "}
+          <br />
+          You will find jobs from <br /> multiple portals such as :
+        </p>
+        <ul className="mt-2">
+          <li>Linkedln</li>
+          <li>Indeed</li>
+          <li>InstaHyre</li>
+          <li>WellFound</li>
+          <li>SimplyHired</li>
+          <li>GlassDoor</li>
+          <li>BeeBee India</li>
+          <li>CutShort</li>
+          <li>SmartRecruiters</li>
+          <li>Shine</li>
+          <li>Mogul</li>
+          <li className="font-[500] text-[18px] ">and many more...</li>
+          <li></li>
+        </ul>
+      </div>
       {/* <SearchResult /> */}
     </div>
   );
